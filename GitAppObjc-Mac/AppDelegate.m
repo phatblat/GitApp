1f324821d02fc111c0a5c9357008fc10ffc7b24c
Ben Chatelain <ben@octop.ad>
2015-08-26 21:36:52 -0600

    Add SSH clone test for private Bitbucket repo

--- GitAppObjc-Mac/AppDelegate.m
+++ GitAppObjc-Mac/AppDelegate.m
@@ -45,6 +45,8 @@
         NSLog(@"git_libgit2_opts returned an error %ld", (long)returnValue);
     }
 
+    // Clone over HTTPS
+
     NSURL *cloneURL = [NSURL URLWithString:@"https://github.com/phatblat/GitApp.git"];
     NSURL *workDirURL = [NSURL URLWithString:@"/tmp/GitApp"];
 
@@ -58,9 +60,9 @@
 
     NSDictionary *options = @{};
     GTRepository *repo = [GTRepository cloneFromURL:cloneURL toWorkingDirectory:workDirURL options:options error:&error
-      transferProgressBlock:^(const git_transfer_progress * _Nonnull progress, BOOL * _Nonnull stop) {
+      transferProgressBlock:^(const git_transfer_progress * __nonnull progress, BOOL * __nonnull stop) {
         NSLog(@"transfer progress %ld/%ld", (long)progress->received_objects, (long)progress->total_objects);
-    } checkoutProgressBlock:^(NSString * _Nonnull path, NSUInteger completedSteps, NSUInteger totalSteps) {
+    } checkoutProgressBlock:^(NSString * __nonnull path, NSUInteger completedSteps, NSUInteger totalSteps) {
         NSLog(@"clone progress %ld/%ld", (long)completedSteps, (long)totalSteps);
     }];
 
@@ -69,6 +71,48 @@
         return;
     }
     NSLog(@"Cloned repo: %@", repo.description);
+
+
+    // Clone over SSH
+                                        // ssh://git@bitbucket.org/dump_rs/gullivr.git
+    cloneURL = [NSURL URLWithString:@"git@bitbucket.org:phatblat/octopad.git"];
+    workDirURL = [NSURL URLWithString:@"/tmp/Octopad"];
+
+    __block GTCredentialProvider *provider = nil;
+    provider = [GTCredentialProvider providerWithBlock:^GTCredential *(GTCredentialType type, NSString *URL, NSString *credUserName) {
+        GTCredential *cred = nil;
+        NSError *error = nil;
+
+        // No passphrase
+//        NSURL *public = [NSURL fileURLWithPath:[@"~/.ssh/id_rsa.pub" stringByExpandingTildeInPath]];
+//        NSURL *private = [NSURL fileURLWithPath:[@"~/.ssh/id_rsa" stringByExpandingTildeInPath]];
+//        NSString *passphrase = nil;
+
+        // Passphrase
+        NSURL *public = [NSURL fileURLWithPath:[@"~/.ssh/objective-git-test_rsa.pub" stringByExpandingTildeInPath]];
+        NSURL *private = [NSURL fileURLWithPath:[@"~/.ssh/objective-git-test_rsa" stringByExpandingTildeInPath]];
+        NSString *passphrase = @"demopass123";
+
+        cred = [GTCredential credentialWithUserName:@"git"
+                                       publicKeyURL:public
+                                      privateKeyURL:private
+                                         passphrase:passphrase
+                                              error:&error];
+        
+        
+        return cred;
+    }];
+
+    error = nil;
+    repo = [GTRepository cloneFromURL:cloneURL toWorkingDirectory:workDirURL options:@{GTRepositoryCloneOptionsCredentialProvider: provider} error:&error
+                transferProgressBlock:^(const git_transfer_progress * __nonnull progress, BOOL * __nonnull stop) {
+                    NSLog(@"transfer progress %ld/%ld", (long)progress->received_objects, (long)progress->total_objects);
+                } checkoutProgressBlock:^(NSString * __nonnull path, NSUInteger completedSteps, NSUInteger totalSteps) {
+                    NSLog(@"clone progress %ld/%ld", (long)completedSteps, (long)totalSteps);
+                }];
+    if (!repo) {
+        NSLog(@"Error cloning private repo over SSH: %@", error);
+    }
 }
 
 - (void)applicationWillTerminate:(NSNotification *)notification
